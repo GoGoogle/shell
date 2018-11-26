0bab485196240a9d5b713e5c520398e28dcb99e1
GoGoogle <wowjoint@gmail.com>
2018-11-27 00:53:25 +0800

    Update testip.sh
    默认只显示低于200ms的ok结果

--- testip.sh
+++ testip.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 ## confirm params
-# PING 4 次，取平均值res 的整数num，小于50ms的显示ok，否则显示faill。
+# PING 4 次，取平均值res 的整数num，小于200ms的显示ok，否则显示faill。
 # 用法：testip.sh ip.txt列表文件（一行一个IPV4）
 if [ `echo $* | awk '{print NF}'` -eq 0 ];then
 echo "please select a file!"
@@ -22,10 +22,11 @@ for line in `cat $file`
 do
 res=`ping -c 4 $line|grep rtt|awk '{print $4}'|awk -F "/" '{print $2}'`
 num=${res%.*}
-if [ $num -lt 50 ];then
+if [ $num -lt 200 ];then
 echo ping \"$line\" \t\t ok $num\m\s
-# 默认只显示低于50ms的ok结果，去掉下列两行注释即可得到所有结果
-# else
-# echo ping \"$line\" \t\t fail $num\m\s
 fi
 done
+
+# 默认只显示低于200ms的ok结果，去掉下列两行注释即可得到所有结果，并添加 至最后一个fi之前
+# else
+# echo ping \"$line\" \t\t fail $num\m\s
