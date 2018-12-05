4e73d5f10bd7a2bfd40098cbbd7058ec025d6430
GoGoogle <wowjoint@gmail.com>
2018-12-05 11:10:56 +0800

    Create eset_index.php
    更新主页

---
+++ eset_index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="zh-cn">
+
+<head>
+<title>离线更新病毒库_ESET OFFLINE UPDATE_By GoGoogle 2018-4-16</title>
+<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+</head>
+
+<body>
+感谢老毛子<sup>Ivanovo State University</sup>提供最新的病毒库！每周一、四、六获取更新包。<hr />
+<?php
+$filename = '/home/bmwcto/soft/list/offline_update_eav.zip';
+echo '创建时间：'.date("Y-m-d H:i:s",filectime($filename)).'<br />';
+echo '修改时间：'.date("Y-m-d H:i:s",filemtime($filename));
+?>
+<hr />
+By GoGoogle 有任何疑问，请联系：X#X
+</body></html>
