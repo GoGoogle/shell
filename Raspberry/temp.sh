242f0853c2cc1d684ed94a616877044b796be55a
GoGoogle <wowjoint@gmail.com>
2019-03-02 16:47:37 +0800

    Update temp.sh

--- Raspberry/temp.sh
+++ Raspberry/temp.sh
@@ -1,3 +1,5 @@
-##/bash/bin
+
 ##10秒更新显示-当前时间-温度-启动时间
-while : ;do clear;date&uptime&vcgencmd measure_temp; sleep 10; done;
+##/bash/bin
+
+while : ;do clear;echo `date '+%Y-%m-%d %H:%M:%S'`&uptime&vcgencmd measure_temp; sleep 10; done;
