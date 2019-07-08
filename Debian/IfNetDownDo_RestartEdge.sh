2dde82475eb003c5b5e7fcb5a4e050c0f3294ba4
GoGoogle <wowjoint@gmail.com>
2019-07-08 20:17:03 +0800

    Update IfNetDownDo_RestartEdge.sh

--- Debian/IfNetDownDo_RestartEdge.sh
+++ Debian/IfNetDownDo_RestartEdge.sh
@@ -10,15 +10,16 @@ ip=$1
 
 while true
 do
-        pinglog=`ping -c 5 $ip | grep received | awk '{print $1,$4}'`
-        ##每5秒执行一次，取发送的包数和接收的包数
+        pinglog=`ping -c 3 $ip | grep received | awk '{print $1,$4}'`
+        ##执行3次，取发送的包数和接收的包数
         send=`echo $pinglog | awk '{print $1}'`
         ##取发送的包数
         received=`echo $pinglog | awk '{print $2}'`
         ##取接收到的包数
         lost=$(($send-$received))
         ##计算丢失的包数
-        if [ $lost -gt 0 ] ;
+        if [ $lost -eq 3 ] ;
+	##如果发送和接收相差为3，证明3次都没通
                 then
                         bash /root/restartEdge.sh
 						##echo "TSET"
