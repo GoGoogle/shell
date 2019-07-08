f36ffbd00c8469b4c81b06c9000d5d870ecdfc5d
GoGoogle <wowjoint@gmail.com>
2019-07-08 19:02:54 +0800

    Create IfNetDownDo_RestartEdge.sh

---
+++ Debian/IfNetDownDo_RestartEdge.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+##判断目标IP断开时，执行RestartEdge.sh
+##为文件添加执行权限：chmod a+x IfNetDownDo_RestartEdge.sh
+##执行格式为：IfNetDownDo_RestartEdge.sh 被检测的IP地址
+## 2019-07-08 18:39:26
+
+ip=$1
+##被监测的网络IP
+
+while true
+do
+        pinglog=`ping -c 5 $ip | grep received | awk '{print $1,$4}'`
+        ##每5秒执行一次，取发送的包数和接收的包数
+        send=`echo $pinglog | awk '{print $1}'`
+        ##取发送的包数
+        received=`echo $pinglog | awk '{print $2}'`
+        ##取接收到的包数
+        lost=$(($send-$received))
+        ##计算丢失的包数
+        if [ $lost -gt 0 ] ;
+                then
+                        bash /root/restartEdge.sh
+						##echo "TSET"
+                        ##目标IP不通时，执行RestartEdge.sh
+        fi
+		break # 退出循环
+done
