c786e4d560db4db77482055ac13d11fdea583142
GoGoogle <wowjoint@gmail.com>
2019-07-26 23:10:57 +0800

    监控端口连接情况
    还需要再优化

---
+++ Debian/00.ss.Files/port2html.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+## 监控端口连接情况
+## 写入HTML文件
+## 使用: chmod a+x ./port2html.sh 443 /var/www/html
+## 每3分钟刷新一次443端口
+## */3 * * * * /root/port2html.sh 443 /var/www/html
+## by bmwcto 2019.7.26 23:01
+
+port=$1
+htmlfile=$2/$port.html
+Pstatus="<pre> `netstat -ntu | grep :$port | awk '{print $4,$5}'| sort | uniq -c | sort -n`</pre>"
+echo "$Pstatus at `date '+%Y-%m-%d %H:%M:%S'`" >$htmlfile;
