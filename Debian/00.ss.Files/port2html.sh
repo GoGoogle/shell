f765e5f6f5e57b5b31f123f6e245752f1d73ba0c
GoGoogle <wowjoint@gmail.com>
2019-07-28 13:45:33 +0800

    添加使用说明，在无参数时输出使用说明

--- Debian/00.ss.Files/port2html.sh
+++ Debian/00.ss.Files/port2html.sh
@@ -6,12 +6,36 @@
 ## */3 * * * * /root/port2html.sh 443 /var/www/html
 ## by bmwcto 2019.7.26 23:01
 
-ver=3.5
+ver=3.6
 port=$1
 htmlfile=$2/$port.html
 txtfile=$2/$port.txt
 xtime=`date '+%Y-%m-%d %H:%M:%S'`
 
+## 使用说明
+  4 usage () {
+  3         name=`basename $0`
+  2         echo "$name 版本：$ver by <bmwcto>"
+  1         echo
+20          echo "用处：记录本机第1个内网网卡的特定端口被访问的IP地址，并存入指定路径，最后过滤提取所有IP到指定文件，方便查阅。"
+  1         echo
+  2         echo "使用说明如下："
+  3         echo
+  4         echo "./$name 端口 文件存放路径"
+  5         echo
+  6         echo "例如："
+  7         echo "./$name 443 /var/www/html"
+  8         echo
+  9 }
+ 10
+ 11 ## 未加任何参数时输出使用帮助
+ 12 shift `expr $OPTIND - 1`
+ 13
+ 14 if [ "x$1" = "x" ]; then
+ 15         usage
+ 16         exit
+ 17 fi
+ 
 #### 获取本机内网IP的两种方式
 #- `ip a|grep -w 'inet'|grep 'global'|sed 's/.*inet.//g'|sed 's/\/[0-9][0-9].*$//g'`
 #- `ip -4 addr|sed -n -e '/brd/p'|sed -n -e 's/\/.*$//p'|awk '{print $2}'`
