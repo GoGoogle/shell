c542453ef7f3d34dcc4f345ff2ec6a9f8401ef44
GoGoogle <wowjoint@gmail.com>
2019-12-04 14:54:55 +0800

    Update one_key_mtg.sh

--- MTProxy/one_key_mtg.sh
+++ MTProxy/one_key_mtg.sh
@@ -1,5 +1,7 @@
-```
 #!/bin/bash
+#一键脚本
+#by BMWCTO 14:54 2019/12/4
+#使用方式：./one_key_mtg.sh 端口
 
 if [ "$#" -eq 0 ]; then
     echo "Usage: $(basename $0) <port>"
@@ -10,4 +12,4 @@ wget -O mtg https://github.com/9seconds/mtg/releases/download/v1.0.1/mtg-linux-a
 pass=$(./mtg generate-secret secured)
 ./mtg run -b0.0.0.0:$1 ${pass} >/dev/null &
 echo ${pass}
-```
+
